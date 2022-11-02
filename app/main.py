b91fdb9559b383ba3d8c83f4fa50dbdd60fe8686
YG <yves.guimard@gmail.com>
2022-11-02 10:19:43 +0100

    Work on messages interface

--- app/main.py
+++ app/main.py
@@ -43,17 +43,19 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def main(request: Request, db: Session = Depends(get_db)):
     """Home page get request"""
+    alert = {"success": "","danger": "","warning": ""}
     client_host = request.client.host
     user = await check_user(request, db)
     return templates.TemplateResponse(
         "home.html",
-        {"request": request, "client_host": client_host, "user": user})
+        {"request": request, "client_host": client_host, "user": user, "alert": alert})
 
 @app.get("/signin")
 async def get_signin(request: Request, db: Session = Depends(get_db)):
     """Signin get request"""
+    alert = {"success": "","danger": "","warning": ""}
     user = await check_user(request, db)
-    return templates.TemplateResponse("signin.html", {"request": request, "user": user})
+    return templates.TemplateResponse("signin.html", {"request": request, "user": user, "alert": alert})
 
 @app.post("/signin")
 def post_signin(
@@ -61,53 +63,95 @@ def post_signin(
     db: Session = Depends(get_db),
     form_data: OAuth2PasswordRequestForm = Depends() ):
     """Signin post request"""
+    alert = {"success": "","danger": "","warning": ""}
     client_host = request.client.host
     user = authenticate(email=form_data.username, password=form_data.password, db=db)
-    response = templates.TemplateResponse(
-        "home.html",
-        {"request": request, "client_host": client_host, "user": user})
-
+    
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")  # 3
+        alert["warning"] = "Incorrect username or password"
+        #raise HTTPException(status_code=400, detail="Incorrect username or password")  # 3
+        response = templates.TemplateResponse(
+        "signin.html",
+        {"request": request, "client_host": client_host, "user": user, "alert": alert}) 
+        return response
+
     access_token = create_access_token(sub=user.id)
     encoded_token = encoders.jsonable_encoder(access_token)
-    response.set_cookie(
-    key="access_token",
-    value=f"Bearer {encoded_token}",
-    httponly=True)
+    
     for scope in form_data.scopes:
         if scope == "cli":
             return {
             "access_token": access_token
             }
+    alert["success"] = "Signin success"
+    response = templates.TemplateResponse(
+        "home.html",
+        {"request": request, "client_host": client_host, "user": user, "alert": alert})
+    response.set_cookie(
+    key="access_token",
+    value=f"Bearer {encoded_token}",
+    httponly=True)
+
     return response
 
 @app.get("/logout")
 def logout(response : Response,request: Request):
     """logout get request"""
-
+    alert = {"success": "","danger": "","warning": ""}
     client_host = request.client.host
     user = ""
     #response = RedirectResponse('/signin', status_code= 302)
+
+    alert["success"] = "Logout successfull"
     response = templates.TemplateResponse("home.html",
-    {"request": request, "client_host": client_host, "user": user})
+    {"request": request, "client_host": client_host, "user": user, "alert": alert})
     response.delete_cookie(key ='access_token')
+  
+
     return response
 
 @app.get("/register")
 async def register(request: Request, db: Session = Depends(get_db)):
     """register get request"""
+    alert = {"success": "","danger": "","warning": ""}
     user = await check_user(request, db)
-    return templates.TemplateResponse("register.html", {"request": request, "user": user})
+    return templates.TemplateResponse("register.html", {"request": request, "user": user, "alert": alert})
 
 @app.post("/register")
-def create_user(email: str = Form(), password: str = Form(), db: Session = Depends(get_db)):
+def post_register(request: Request, email: str = Form(), password: str = Form(), confirm_password: str = Form(), db: Session = Depends(get_db)):
     """register post request"""
+    alert = {"success": "","danger": "","warning": ""}
+
     db_user = crud.get_user_by_email(db, email)
+    client_host = request.client.host
+    user = ""
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    user = schemas.UserCreate(email=email, is_admin=False, password=password)
-    return crud.create_user(db=db, user=user)
+        alert["warning"] = "Email already registered."
+        response = templates.TemplateResponse(
+        "register.html",
+        {"request": request, "client_host": client_host, "user": user, "alert": alert}) 
+        return response
+    if confirm_password != password:
+        alert["warning"] = "Password missmatch!"
+        response = templates.TemplateResponse(
+        "register.html",
+        {"request": request, "client_host": client_host, "user": user, "alert": alert}) 
+        return response
+    
+    if schemas.UserCreate(email=email, is_admin=False, password=password):
+        alert["success"] = "Account creation successfull, you can now signin."
+    else:
+        alert["danger"] = "An error occured during your account creation."
+        response = templates.TemplateResponse(
+        "register.html",
+        {"request": request, "client_host": client_host, "user": user, "alert": alert}) 
+        return response
+    
+    response = templates.TemplateResponse(
+        "signin.html",
+        {"request": request, "client_host": client_host, "user": user, "alert": alert})
+
+    return response
 
 
 @app.get("/get_users/", response_model=list[schemas.User])
