974e9d24bcf67f62e9a4c4a2c534875b83f89acd
YG <yves.guimard@gmail.com>
2024-03-21 18:24:58 +0100

    Fix schemas.py for pydantic V2

--- app/schemas.py
+++ app/schemas.py
@@ -1,27 +1,25 @@
 from typing import Union, Optional
 
-from pydantic import BaseModel, IPvAnyAddress, EmailStr, AnyUrl
+from pydantic import ConfigDict, BaseModel, IPvAnyAddress, EmailStr, AnyUrl
 
 class InstantAccess(BaseModel):
-    id: Optional[int]
-    owner_id: Optional[int]
+    id: Optional[int] = None
+    owner_id: Optional[int] = None
     link: AnyUrl
     unique_link: str = None
     description: Union[str, None] = None
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 class InstantAccessCreate(InstantAccess):
     pass
 
 class Ip(BaseModel):
-    id: Optional[int]
-    owner_id: Optional[int]
+    id: Optional[int] = None
+    owner_id: Optional[int] = None
     value: IPvAnyAddress
     description: Union[str, None] = None
     origin: str = None
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 class IpCreate(Ip):
     pass
@@ -33,8 +31,7 @@ class User(BaseModel):
     email: EmailStr
     is_admin: Optional[bool] = False
     twofactor: str = None
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 class UserCreate(User):
     password: str
@@ -43,13 +40,12 @@ class TokenData(BaseModel):
     username: Optional[str] = None
 
 class Token(BaseModel):
-    id: Optional[int]
-    owner_id: Optional[int]
+    id: Optional[int] = None
+    owner_id: Optional[int] = None
     key: str = None
     secret: str = None
     description: Union[str, None] = None
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 class TokenCreate(Token):
     pass
